bf2a084f70984f45f2f71e05df55e4ac07cc31e6
Evan Lin <evanslin@gmail.com>
2018-07-29 00:59:31 +0800

    gokit

---
+++ introduction-channel/gokit-channel.go
@@ -0,0 +1,48 @@
+// +build OMIT
+
+import (
+	"context"
+	"sync"
+
+	"github.com/go-kit/kit/endpoint"
+)
+
+func ImagePreprocessingPipeline(numOfDatasetLoaders)	{
+	pipeline := endpoint.Chain(
+	// CPU-bound + IO-bound task
+	loadImageDataset(g.sp.Mongo, numOfDatasetLoaders),
+
+	// IO-bound task
+	readImages(numOfImageReaders),
+
+	// CPU-bound task
+	decodeImages(numOfImageDecoders),
+
+	// IO-bound task
+	dumpImages(dataDir, numOfImageWriters),
+	//...
+	)(progressUpdaterEndpoint)
+}
+
+func readImages(numOfImageReaders int) endpoint.Middleware {
+	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+			in := request.(chan ImageEntry)
+			// channel for loaded images
+			out := make(chan ImageEntry, 1000)
+			go func() {
+				wg := sync.WaitGroup{}
+				for i := 0; i < numOfImageReaders; i++ {
+					wg.Add(1)
+					go func() {
+						defer wg.Done()
+						ImageReader(in, out)
+					}()
+				}
+				wg.Wait()
+				close(out)
+			}()
+			return next(ctx, out)
+		}
+	}
+}
