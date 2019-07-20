52e89f62c88e3404b209946a22b7f29a7897a551
Evan <evanslin@gmail.com>
2019-07-21 02:49:57 +0800

    u

---
+++ introduction-channel-2019/fan-in-out-buffer.go
@@ -0,0 +1,65 @@
+// +build OMIT
+
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int, len(nums))
+	for _, n := range nums {
+		out <- n
+	}
+	close(out)
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n := range in {
+			out <- n * n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func merge(cs ...<-chan int) <-chan int {
+	var wg sync.WaitGroup
+	out := make(chan int)
+
+	output := func(c <-chan int) {
+		for n := range c {
+			out <- n
+		}
+		wg.Done()
+	}
+	wg.Add(len(cs))
+	for _, c := range cs {
+		go output(c)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	c := gen(2, 3)
+
+	// Fan-out
+	c1 := sq(c)
+	c2 := sq(c)
+
+	// Fan-in: Consume the merged output from c1 and c2.
+	for n := range merge(c1, c2) {
+		fmt.Println(n) // 4 then 9, or 9 then 4
+	}
+}
